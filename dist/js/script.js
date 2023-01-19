a4e4d91406f696f5bbcae96e68f0f7e2141af47e
PLuIIIkin <viazovskih@rambler.ru>
2023-01-19 22:09:35 +0800

    deletHerni

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,23 +1,19 @@
 const wrap = document.querySelectorAll('.price__item-wrapper');
-    
+
 wrap.forEach((cont) => {
-    let more = cont.querySelector('.price__btn');
-    let closeBtn = cont.querySelector('.price__btn-close');
-    let content = cont.querySelector('.price__item-content');
-    let contentList = cont.querySelector('.price__item-list');
+  let more = cont.querySelector('.price__btn');
+  let closeBtn = cont.querySelector('.price__btn-close');
+  let content = cont.querySelector('.price__item-content');
+  let contentList = cont.querySelector('.price__item-list');
 
-    more.addEventListener('click', () => {
-        content.classList.remove('price__item-content_active');
-        contentList.classList.add('price__item-list_active');
-    });
-    
-    closeBtn.addEventListener('click', () => {
-        contentList.classList.remove('price__item-list_active');
-        content.classList.add('price__item-content_active');
-    });
-})
+  more.addEventListener('click', () => {
+    content.classList.remove('price__item-content_active');
+    contentList.classList.add('price__item-list_active');
+  });
 
-$(document).ready(function(){
-    $('.feedback__iner').slick({
-    });
+  closeBtn.addEventListener('click', () => {
+    contentList.classList.remove('price__item-list_active');
+    content.classList.add('price__item-content_active');
+  });
 });
+
